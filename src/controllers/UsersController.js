e03fcd314c2e2b51ef02a38f80ffafe5ca2d0cd8
Francisco <francar76@hotmail.com>
2023-01-09 15:36:57 -0300

    atualized

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
 class UsersController {
@@ -24,7 +24,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -41,17 +41,34 @@ class UsersController {
     if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError('this email is already in use')
     }
-    user.name = name
-    user.email = email
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError(
+        'you need to enter the old password to set the new password!'
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+      if (!checkOldPassword) {
+        throw new AppError('password does not match')
+      }
+      user.password = await hash(password, 8)
+    }
+
     await database.run(
-      `UPDATE users SET 
+      `
+    UPDATE users SET 
       name = ?, 
       email = ?, 
-      updated_at = ?, 
+      password = ?,
+      updated_at = DATETIME('now')
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, id]
     )
-    return response.json()
+    return response.status(201).json()
   }
 }
 
